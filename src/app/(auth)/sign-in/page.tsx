76200c989ed11eec98a3233f78e0ba2ef70a4c9c
Mosayeb09 <mosayebhossain09@gmail.com>
2025-01-04 19:39:15 +0600

    creating sign-up page

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,64 +1,11 @@
-'use client'
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
-import * as z from "zod"
-import Link from "next/link"
-import { useState, useEffect } from "react"
-import { useDebounceValue } from 'usehooks-ts'
-import { useToast } from "@/hooks/use-toast"
-import { signupSchema } from "@/schemas/signUpSchema"
-import { set } from "mongoose"
-import axios, { AxiosError } from "axios"
-import { ApiResponse } from "@/types/apiResponse"
-
+import React from 'react';
 
 const page = () => {
-    const [userName, setUserName] = useState("");
-    const [userNameMessage, setUserNameMessage] = useState("");
-    const [isCheckingUsername, setIsCheckingUsername] = useState(false);
-    const [isSubmitting, setIsSubmitting] = useState(false);
-    const debouncedUserName = useDebounceValue(userName, 300);
-    const { toast } = useToast();
-    const router = useRouter();
-
-    //zod implementation
-    const form = useForm<z.infer<typeof signupSchema>({
-        resolver:zodResolver(signupSchema),
-        defaultValues:{
-            username:"",
-            email:"",
-            password:""
-        }
-    })
-    useEffect(() => {
-        const checkUserNameUniqueness = async () => {
-            if(debouncedUserName){
-                setIsCheckingUsername(true);
-                setUserNameMessage("");
-                try {
-                  const  response = await axios.get(`/api/check-username-unique?username=${debouncedUserName}`);
-                  setUserNameMessage(response.data.message);
-            }
-            catch(error){
-                const axiosError = error as AxiosError<ApiResponse>;
-                setUserNameMessage(axiosError.response?.data.message??"Error checking username");
-                
-
-            }
-            finally{
-                setIsCheckingUsername(false);
-            }
-            
-        }
-    }
-    checkUserNameUniqueness();}, [debouncedUserName])
-    return (
-        <div>
-        
-        </div>
-    );
+  return (
+    <div>
+      
+    </div>
+  );
 };
 
 export default page;
-
-
