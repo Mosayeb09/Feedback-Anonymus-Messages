b6ecdb24793e7572547d7c0d8bd90fec8ddf7051
Mosayeb09 <mosayebhossain09@gmail.com>
2025-01-08 18:57:06 +0600

    fixing bug in dashboard page

--- src/app/(app)/dashboard/page.tsx
+++ src/app/(app)/dashboard/page.tsx
@@ -8,29 +8,23 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import axios, { AxiosError } from "axios";
 
 import { useSession } from "next-auth/react";
-
+import { useCallback, useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 
 import { Button } from "@/components/ui/button";
 import { Switch } from "@/components/ui/switch";
 import { Separator } from "@/components/ui/separator";
 import { Loader2, RefreshCcw } from "lucide-react";
-import { MessageCard } from '@/components/messageCard'
-import { useState } from "react";
+import {MessageCard} from "@/components/MessageCard";
 
-
-
-const page = () => {
-//   const [message, setMessage] = useState<Message[]>([]);
-//   const [isLoading, setIsLoading] = useState(false);
-//   const [isSwitchLoding, setIsSwitchLoding] = useState(false);
-
-const [messages, setMessages] = useState<Message[]>([]);
+const Page = () => {
+  const [messages, setMessages] = useState<Message[]>([]);
   const [isLoading, setIsLoading] = useState(false);
   const [isSwitchLoading, setIsSwitchLoading] = useState(false);
+
   const { toast } = useToast();
   const handleDeleteMessage = async (messageId: string) => {
-    setMessage(message.filter((message) => message._id !== messageId));
+    setMessages(messages.filter((message) => message._id !== messageId));
   };
   const { data: session } = useSession();
 
@@ -39,8 +33,9 @@ const [messages, setMessages] = useState<Message[]>([]);
   });
   const { register, watch, setValue } = form;
   const acceptMessages = watch("acceptMessages");
+
   const fetchAcceptedMessages = useCallback(async () => {
-    setIsSwitchLoding(true);
+    setIsSwitchLoading(true);
     try {
       const response = await axios.get<ApiResponse>("/api/accept-message");
       setValue("acceptMessages", response.data.isAcceptingMessage);
@@ -48,55 +43,46 @@ const [messages, setMessages] = useState<Message[]>([]);
       const axiosError = error as AxiosError<ApiResponse>;
       toast({
         title: "Error",
-        description:
-          axiosError.response?.data.message ||
-          "Faild to fetch message settings",
+        description: axiosError.response?.data.message || "Failed to fetch message settings",
         variant: "destructive",
       });
     } finally {
-      setIsSwitchLoding(false);
+      setIsSwitchLoading(false);
     }
-  }, [setValue]);
+  }, [setValue, toast]);
 
-  const fetchMessages = useCallback(
-    async (refresh: boolean = false) => {
-      setIsLoading(true);
-      setIsSwitchLoding(false);
-      try {
-        const response = await axios.get<ApiResponse>("/api/get-messages");
-        setMessage(response.data.messages || []);
-        if (refresh) {
-          toast({
-            title: "Refreshed Messages",
-            description: "Showing latest messages",
-            variant: "destructive",
-          });
-        }
-      } catch (error) {
-        const axiosError = error as AxiosError<ApiResponse>;
+  const fetchMessages = useCallback(async (refresh = false) => {
+    setIsLoading(true);
+    try {
+      const response = await axios.get<ApiResponse>("/api/get-messages");
+      setMessages(response.data.messages || []);
+      if (refresh) {
         toast({
-          title: "Error",
-          description:
-            axiosError.response?.data.message || "Faild to fetch messages",
+          title: "Messages Refreshed",
+          description: "Showing the latest messages",
           variant: "destructive",
         });
-      } finally {
-        setIsLoading(false);
-        setIsLoading(false);
       }
-    },
-    [setIsLoading, setMessage]
-  );
+    } catch (error) {
+      const axiosError = error as AxiosError<ApiResponse>;
+      toast({
+        title: "Error",
+        description: axiosError.response?.data.message || "Failed to fetch messages",
+        variant: "destructive",
+      });
+    } finally {
+      setIsLoading(false);
+      setIsSwitchLoading(false);
+    }
+  }, [setIsLoading, setMessages,toast]);
 
   useEffect(() => {
-    if (!session || !session.user) {
-      return;
+    if ( !session||!session?.user) {
       fetchMessages();
       fetchAcceptedMessages();
     }
-  }, [session, setValue, fetchMessages, fetchAcceptedMessages]);
+  }, [session, fetchMessages, fetchAcceptedMessages, toast,setValue]);
 
-  //handle switch change
   const handleSwitchChange = async () => {
     try {
       const response = await axios.post("/api/accept-message", {
@@ -104,48 +90,49 @@ const [messages, setMessages] = useState<Message[]>([]);
       });
       setValue("acceptMessages", !acceptMessages);
       toast({
-        title: response.data.message,
-        variant: "destructive",
+        title: response.data.messages,
+        variant: "default",
       });
     } catch (error) {
       const axiosError = error as AxiosError<ApiResponse>;
       toast({
         title: "Error",
-        description:
-          axiosError.response?.data.message || "Faild to fetch messages",
+        description: axiosError.response?.data.message ?? "Failed to update settings",
         variant: "destructive",
       });
     }
   };
-  const { username } = session?.user;
-  //DO more research
-  const baseUrl = `${window.location.protocol}//${window.location.host}`;
-  const profileUrl = `${baseUrl}/u/${username}`;
-  const copytoClipboard = () => {
+
+  const copyToClipboard = () => {
+    const baseUrl = `${window.location.protocol}//${window.location.host}`;
+    const profileUrl = `${baseUrl}/u/${session?.user?.username}`;
     navigator.clipboard.writeText(profileUrl);
     toast({
-      title: "Copied to clipboard",
-      description: "Copied profile url to clipboard",
+      title: "Copied to Clipboard",
+      description: "Profile URL copied successfully",
     });
   };
+  
+
   if (!session || !session.user) {
     return <div>Please login</div>;
   }
+  
 
   return (
     <div className="my-8 mx-4 md:mx-8 lg:mx-auto p-6 bg-white rounded w-full max-w-6xl">
       <h1 className="text-4xl font-bold mb-4">User Dashboard</h1>
 
       <div className="mb-4">
-        <h2 className="text-lg font-semibold mb-2">Copy Your Unique Link</h2>{" "}
+        <h2 className="text-lg font-semibold mb-2">Copy Your Unique Link</h2>
         <div className="flex items-center">
           <input
             type="text"
-            value={profileUrl}
+            value={`${window.location.protocol}//${window.location.host}/u/${session?.user?.username}`}
             disabled
             className="input input-bordered w-full p-2 mr-2"
           />
-          <Button onClick={copytoClipboard }>Copy</Button>
+          <Button onClick={copyToClipboard}>Copy</Button>
         </div>
       </div>
 
@@ -154,21 +141,19 @@ const [messages, setMessages] = useState<Message[]>([]);
           {...register("acceptMessages")}
           checked={acceptMessages}
           onCheckedChange={handleSwitchChange}
-          disabled={isSwitchLoding}
+          disabled={isSwitchLoading}
         />
         <span className="ml-2">
           Accept Messages: {acceptMessages ? "On" : "Off"}
         </span>
       </div>
+
       <Separator />
 
       <Button
         className="mt-4"
         variant="outline"
-        onClick={(e) => {
-          e.preventDefault();
-          fetchMessages(true);
-        }}
+        onClick={() => fetchMessages(true)}
       >
         {isLoading ? (
           <Loader2 className="h-4 w-4 animate-spin" />
@@ -176,14 +161,15 @@ const [messages, setMessages] = useState<Message[]>([]);
           <RefreshCcw className="h-4 w-4" />
         )}
       </Button>
+
       <div className="mt-4 grid grid-cols-1 md:grid-cols-2 gap-6">
-        {message.length > 0 ? (
-          message.map((message, index) => (
+        {messages.length > 0 ? (
+          messages.map((message) => (
             <MessageCard
-              key={message._id}
-              message={message}
-              onMessageDelete={handleDeleteMessage}
-            />
+            key={message._id}
+            message={message}
+            onMessageDelete={handleDeleteMessage}
+          />
           ))
         ) : (
           <p>No messages to display.</p>
@@ -193,7 +179,204 @@ const [messages, setMessages] = useState<Message[]>([]);
   );
 };
 
-export default page;
+export default Page;
+
+// "use client";
+
+// import { useToast } from "@/hooks/use-toast";
+// import { Message } from "@/model/user";
+// import { acceptMessageSchema } from "@/schemas/acceptMessageSchema";
+// import { ApiResponse } from "@/types/apiResponse";
+// import { zodResolver } from "@hookform/resolvers/zod";
+// import axios, { AxiosError } from "axios";
+
+// import { useSession } from "next-auth/react";
+
+// import { useForm } from "react-hook-form";
+
+// import { Button } from "@/components/ui/button";
+// import { Switch } from "@/components/ui/switch";
+// import { Separator } from "@/components/ui/separator";
+// import { Loader2, RefreshCcw } from "lucide-react";
+// import { MessageCard } from '@/components/messageCard'
+// import { useState } from "react";
+
+
+
+// const page = () => {
+// //   const [message, setMessage] = useState<Message[]>([]);
+// //   const [isLoading, setIsLoading] = useState(false);
+// //   const [isSwitchLoding, setIsSwitchLoding] = useState(false);
+
+// const [messages, setMessages] = useState<Message[]>([]);
+//   const [isLoading, setIsLoading] = useState(false);
+//   const [isSwitchLoading, setIsSwitchLoading] = useState(false);
+//   const { toast } = useToast();
+//   const handleDeleteMessage = async (messageId: string) => {
+//     setMessage(message.filter((message) => message._id !== messageId));
+//   };
+//   const { data: session } = useSession();
+
+//   const form = useForm({
+//     resolver: zodResolver(acceptMessageSchema),
+//   });
+//   const { register, watch, setValue } = form;
+//   const acceptMessages = watch("acceptMessages");
+//   const fetchAcceptedMessages = useCallback(async () => {
+//     setIsSwitchLoding(true);
+//     try {
+//       const response = await axios.get<ApiResponse>("/api/accept-message");
+//       setValue("acceptMessages", response.data.isAcceptingMessage);
+//     } catch (error) {
+//       const axiosError = error as AxiosError<ApiResponse>;
+//       toast({
+//         title: "Error",
+//         description:
+//           axiosError.response?.data.message ||
+//           "Faild to fetch message settings",
+//         variant: "destructive",
+//       });
+//     } finally {
+//       setIsSwitchLoding(false);
+//     }
+//   }, [setValue]);
+
+//   const fetchMessages = useCallback(
+//     async (refresh: boolean = false) => {
+//       setIsLoading(true);
+//       setIsSwitchLoding(false);
+//       try {
+//         const response = await axios.get<ApiResponse>("/api/get-messages");
+//         setMessage(response.data.messages || []);
+//         if (refresh) {
+//           toast({
+//             title: "Refreshed Messages",
+//             description: "Showing latest messages",
+//             variant: "destructive",
+//           });
+//         }
+//       } catch (error) {
+//         const axiosError = error as AxiosError<ApiResponse>;
+//         toast({
+//           title: "Error",
+//           description:
+//             axiosError.response?.data.message || "Faild to fetch messages",
+//           variant: "destructive",
+//         });
+//       } finally {
+//         setIsLoading(false);
+//         setIsLoading(false);
+//       }
+//     },
+//     [setIsLoading, setMessage]
+//   );
+
+//   useEffect(() => {
+//     if (!session || !session.user) {
+//       return;
+//       fetchMessages();
+//       fetchAcceptedMessages();
+//     }
+//   }, [session, setValue, fetchMessages, fetchAcceptedMessages]);
+
+//   //handle switch change
+//   const handleSwitchChange = async () => {
+//     try {
+//       const response = await axios.post("/api/accept-message", {
+//         acceptMessage: !acceptMessages,
+//       });
+//       setValue("acceptMessages", !acceptMessages);
+//       toast({
+//         title: response.data.message,
+//         variant: "destructive",
+//       });
+//     } catch (error) {
+//       const axiosError = error as AxiosError<ApiResponse>;
+//       toast({
+//         title: "Error",
+//         description:
+//           axiosError.response?.data.message || "Faild to fetch messages",
+//         variant: "destructive",
+//       });
+//     }
+//   };
+//   const { username } = session?.user;
+//   //DO more research
+//   const baseUrl = `${window.location.protocol}//${window.location.host}`;
+//   const profileUrl = `${baseUrl}/u/${username}`;
+//   const copytoClipboard = () => {
+//     navigator.clipboard.writeText(profileUrl);
+//     toast({
+//       title: "Copied to clipboard",
+//       description: "Copied profile url to clipboard",
+//     });
+//   };
+//   if (!session || !session.user) {
+//     return <div>Please login</div>;
+//   }
+
+//   return (
+//     <div className="my-8 mx-4 md:mx-8 lg:mx-auto p-6 bg-white rounded w-full max-w-6xl">
+//       <h1 className="text-4xl font-bold mb-4">User Dashboard</h1>
+
+//       <div className="mb-4">
+//         <h2 className="text-lg font-semibold mb-2">Copy Your Unique Link</h2>{" "}
+//         <div className="flex items-center">
+//           <input
+//             type="text"
+//             value={profileUrl}
+//             disabled
+//             className="input input-bordered w-full p-2 mr-2"
+//           />
+//           <Button onClick={copytoClipboard }>Copy</Button>
+//         </div>
+//       </div>
+
+//       <div className="mb-4">
+//         <Switch
+//           {...register("acceptMessages")}
+//           checked={acceptMessages}
+//           onCheckedChange={handleSwitchChange}
+//           disabled={isSwitchLoding}
+//         />
+//         <span className="ml-2">
+//           Accept Messages: {acceptMessages ? "On" : "Off"}
+//         </span>
+//       </div>
+//       <Separator />
+
+//       <Button
+//         className="mt-4"
+//         variant="outline"
+//         onClick={(e) => {
+//           e.preventDefault();
+//           fetchMessages(true);
+//         }}
+//       >
+//         {isLoading ? (
+//           <Loader2 className="h-4 w-4 animate-spin" />
+//         ) : (
+//           <RefreshCcw className="h-4 w-4" />
+//         )}
+//       </Button>
+//       <div className="mt-4 grid grid-cols-1 md:grid-cols-2 gap-6">
+//         {message.length > 0 ? (
+//           message.map((message, index) => (
+//             <MessageCard
+//               key={message._id}
+//               message={message}
+//               onMessageDelete={handleDeleteMessage}
+//             />
+//           ))
+//         ) : (
+//           <p>No messages to display.</p>
+//         )}
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default page;
 
 
 
--- src/components/messageCard.tsx
+++ src/components/messageCard.tsx
@@ -1,12 +1,11 @@
-"use client";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
+'use client'
+
+import React from 'react';
+import axios, { AxiosError } from 'axios';
+import dayjs from 'dayjs';
+import { X } from 'lucide-react';
+import { Message } from '@/model/user';
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import {
   AlertDialog,
   AlertDialogAction,
@@ -17,62 +16,155 @@ import {
   AlertDialogHeader,
   AlertDialogTitle,
   AlertDialogTrigger,
-} from "@/components/ui/alert-dialog";
-import { Button } from "./ui/button";
-import { X } from "lucide-react";
-import { useToast } from "@/hooks/use-toast";
-import axios from "axios";
-
-
+} from '@/components/ui/alert-dialog';
+import { Button } from './ui/button';
+import { useToast } from '@/hooks/use-toast';
+import { ApiResponse } from '@/types/apiResponse';
 
 type MessageCardProps = {
-    message: Message;
-    onMessageDelete:( messageId:string)=>void
-}
+  messages: Message;
+  onMessageDelete: (messageId: string) => void;
+};
 
-function messageCard({message,onMessageDelete}:MessageCardProps) {
-    const {toast} = useToast()
-    const handleDeleteConfirm = async() => {
-        const response =await axios.delete(`/api/delete-messages/${message._id}`)
-        toast({
-            title:response.data.message
-        })
-        onMessageDelete(message._id)
-        
-    }
+export function MessageCard({ message, onMessageDelete }: MessageCardProps) {
+  const { toast } = useToast();
+
+  const handleDeleteConfirm = async () => {
+    try {
+      const response = await axios.delete<ApiResponse>(
+        `/api/delete-message/${message._id}`
+      );
+      toast({
+        title: response.data.message,
+      });
+      onMessageDelete(message._id);
+
+    } catch (error) {
+      const axiosError = error as AxiosError<ApiResponse>;
+      toast({
+        title: 'Error',
+        description:
+          axiosError.response?.data.message ?? 'Failed to delete message',
+        variant: 'destructive',
+      });
+    } 
+  };
 
   return (
-    <Card>
+    <Card className="card-bordered">
       <CardHeader>
-        <CardTitle>Card Title</CardTitle>
-        <AlertDialog>
-          <AlertDialogTrigger asChild>
-            <Button variant="destructive"><X className="h-4 w-4"/></Button>
-          </AlertDialogTrigger>
-          <AlertDialogContent>
-            <AlertDialogHeader>
-              <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
-              <AlertDialogDescription>
-                This action cannot be undone. This will permanently delete your
-                account and remove your data from our servers.
-              </AlertDialogDescription>
-            </AlertDialogHeader>
-            <AlertDialogFooter>
-              <AlertDialogCancel>Cancel</AlertDialogCancel>
-              <AlertDialogAction onClick={handleDeleteConfirm}>Continue</AlertDialogAction>
-            </AlertDialogFooter>
-          </AlertDialogContent>
-        </AlertDialog>
-        <CardDescription>Card Description</CardDescription>
+        <div className="flex justify-between items-center">
+          <CardTitle>{message.content}</CardTitle>
+          <AlertDialog>
+            <AlertDialogTrigger asChild>
+              <Button variant='destructive'>
+                <X className="w-5 h-5" />
+              </Button>
+            </AlertDialogTrigger>
+            <AlertDialogContent>
+              <AlertDialogHeader>
+                <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+                <AlertDialogDescription>
+                  This action cannot be undone. This will permanently delete
+                  this message.
+                </AlertDialogDescription>
+              </AlertDialogHeader>
+              <AlertDialogFooter>
+                <AlertDialogCancel>
+                  Cancel
+                </AlertDialogCancel>
+                <AlertDialogAction onClick={handleDeleteConfirm}>
+                  Continue
+                </AlertDialogAction>
+              </AlertDialogFooter>
+            </AlertDialogContent>
+          </AlertDialog>
+        </div>
+        <div className="text-sm">
+          {dayjs(message.createdAt).format('MMM D, YYYY h:mm A')}
+        </div>
       </CardHeader>
-      <CardContent>
-        <p>Card Content</p>
-      </CardContent>
-      <CardFooter>
-        <p>Card Footer</p>
-      </CardFooter>
+      <CardContent></CardContent>
     </Card>
   );
 }
 
-export default messageCard;
+
+// "use client";
+// import {
+//   Card,
+//   CardContent,
+//   CardDescription,
+//   CardFooter,
+//   CardHeader,
+//   CardTitle,
+// } from "@/components/ui/card";
+// import {
+//   AlertDialog,
+//   AlertDialogAction,
+//   AlertDialogCancel,
+//   AlertDialogContent,
+//   AlertDialogDescription,
+//   AlertDialogFooter,
+//   AlertDialogHeader,
+//   AlertDialogTitle,
+//   AlertDialogTrigger,
+// } from "@/components/ui/alert-dialog";
+// import { Button } from "./ui/button";
+// import { X } from "lucide-react";
+// import { useToast } from "@/hooks/use-toast";
+// import axios from "axios";
+
+
+
+// type MessageCardProps = {
+//     message: Message;
+//     onMessageDelete:( messageId:string)=>void
+// }
+
+// function messageCard({message,onMessageDelete}:MessageCardProps) {
+//     const {toast} = useToast()
+//     const handleDeleteConfirm = async() => {
+//         const response =await axios.delete(`/api/delete-messages/${message._id}`)
+//         toast({
+//             title:response.data.message
+//         })
+//         onMessageDelete(message._id)
+        
+//     }
+
+//   return (
+//     <Card>
+//       <CardHeader>
+//         <CardTitle>Card Title</CardTitle>
+//         <AlertDialog>
+//           <AlertDialogTrigger asChild>
+//             <Button variant="destructive"><X className="h-4 w-4"/></Button>
+//           </AlertDialogTrigger>
+//           <AlertDialogContent>
+//             <AlertDialogHeader>
+//               <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+//               <AlertDialogDescription>
+//                 This action cannot be undone. This will permanently delete your
+//                 account and remove your data from our servers.
+//               </AlertDialogDescription>
+//             </AlertDialogHeader>
+//             <AlertDialogFooter>
+//               <AlertDialogCancel>Cancel</AlertDialogCancel>
+//               <AlertDialogAction onClick={handleDeleteConfirm}>Continue</AlertDialogAction>
+//             </AlertDialogFooter>
+//           </AlertDialogContent>
+//         </AlertDialog>
+//         <CardDescription>Card Description</CardDescription>
+//       </CardHeader>
+//       <CardContent>
+//         <p>Card Content</p>
+//       </CardContent>
+//       <CardFooter>
+//         <p>Card Footer</p>
+//       </CardFooter>
+//     </Card>
+//   );
+// }
+
+// export default messageCard;
